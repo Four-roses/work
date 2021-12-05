f9538c43a51a8ee747e11facdcd8c295f05023b6
Four-roses <921019370@qq.com>
2021-12-05 12:27:48 +0800

    提交

---
+++ src/zuoye/helloworld.java
@@ -0,0 +1,11 @@
+package zuoye;
+
+public class helloworld { 
+    public static void main(String[] args)
+    {
+          System.out.println("Hello World");
+                              
+    }
+
+
+}
